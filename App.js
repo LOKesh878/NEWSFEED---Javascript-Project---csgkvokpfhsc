e14178bba7cf5836cfee47258e3ccbc4d4f0f157
LOKesh878 <137311475+LOKesh878@users.noreply.github.com>
2024-02-05 13:28:49 +0000

    api

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ async function getUsers() {
   try {
     let res = await fetch(url);
     let data = await res.json();
-    return data; // assuming the response is an array or object containing users
+    return data;
   } catch (error) {
     console.log(error);
   }
@@ -12,31 +12,41 @@ async function getUsers() {
 async function renderUsers() {
   try {
     let users = await getUsers();
-    let html = '';
+    
     // Assuming the response is an array
-    users.map(user => {
-      let htmlSegment = `<div class="user">
-                          <div class="detail">
-
-                           <div class="author">
-                           <p>BY <b>${user.author}</b></p>
-                            </div>
-                            <div class="category">
-                            <p>CATEGORY <B>${user.category}</B></p>
-                          </div>
-                          
-                          </div>
-                            <P>${user.content} <a href='${user.url}'>READ MORE</a></P>
-                           
-                        </div>`;
-      html += htmlSegment;
-    });
-
-    let container = document.querySelector('.newsdisplay');
-    container.innerHTML = html;
-  } catch (error) {
+    showNewsArticales(users.filter((user)=>user.category== 'Business'));
+    setTimeout(()=>{
+      showNewsArticales(users.filter((user)=>user.category== 'Politics'))
+    },3000)
+  }
+  catch (error) {
     console.log(error);
   }
 }
 
 renderUsers();
+
+function showNewsArticales(newsList){
+  let html='';
+  newsList.map(user => {
+    let htmlSegment = `<div class="user">
+                        <div class="detail">
+
+                         <div class="author">
+                         <p>BY <b>${user.author}</b></p>
+                          </div>
+                          <div class="category">
+                          <p>CATEGORY <B>${user.category}</B></p>
+                        </div>
+                        
+                        </div>
+                          <P>${user.content} <a href='${user.url}'>READ MORE</a></P>
+                         
+                      </div>`;
+    html += htmlSegment;
+  });
+
+
+  let container = document.querySelector('.newsdisplay');
+  container.innerHTML = html;
+}
